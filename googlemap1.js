d1fd41783b4a468bd8730353790a21a08da9dfe2
willsawyer2609 <wmsawyer2609@gmail.com>
2021-07-20 23:18:51 +1000

    Initial commit.

---
+++ googlemap1.js
@@ -0,0 +1,6 @@
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+    return { width: srcWidth*ratio, height: srcHeight*ratio };
+}
